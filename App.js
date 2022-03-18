4bd5f807bbff62bea6c91e995a1eddc4f1237069
Pedro Perez <pperezi@bch.bancodechile.cl>
2022-03-18 16:59:48 -0300

    complete section 5

--- App.js
+++ App.js
@@ -1,64 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Dimensions,
+  Button,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  ScrollView,
+  FlatList,
+  SectionList
+} from "react-native";
+import { useState, useEffect } from "react";
 
-const Texto = ({style}) => {
-  // const Texto = (props) => {
-  const [texto, setTexto] = useState("Hola Mundo");
-  const actualizaTexto = () => {
-    setTexto('Chao Mundo!!!')
-  };
-  return (
-    <Text style={[styles.text, style]} onPress={actualizaTexto}>{texto}</Text>
-  )
-  // const {texto} = props;
-  // const {children} = props;
-  // return (
-  //   // <Text>{texto}</Text>
-  //   <Text>{children}</Text>
-  // )
-};
+// const {width, height} = Dimensions.get('window').width;
+
 
 export default function App() {
+  // const [text, setText] = useState("Chanchito feliz");
+  // const [submit, setSubmit] = useState("");
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(data => {
+      setUsers(data)
+      setLoading(false)
+    })
+  }, [])
+
+  if(loading) {
+    return <View style={styles.center}><Text>Cargando ....</Text></View>
+  }
+
   return (
     <View style={styles.container}>
-      <Texto style={styles.red}/>
-      <Texto style={styles.green}/>
-      <Texto style={styles.blue}/> 
-       {/* <Texto texto={'Hola Mundo!!!!!'} />
-       <Texto texto={'Adios buena gente!!!!!'} /> */}
-       {/* <Texto>Hola Mundo</Texto>
-       <Texto>Adios Mundo!</Texto> */}
-      <StatusBar style="auto" />
+      <FlatList
+        data={users} 
+        renderItem={({item}) => <Text style={styles.item}>{item.name}</Text>}
+        keyExtractor={item => String(item.id)}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  text: {
-    color: 'white',
-    fontSize: 24,
-    height: 100,
-    width: 100,
-    padding: 10
+  center: { 
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  red: {
-    backgroundColor: 'red',
-    // flex: 1
+  TouchableOpacity: {
+    backgroundColor: "#eee",
   },
-  green: {
-    backgroundColor: 'green',
-    // flex: 2
+  view: {
+    height: 40,
+    width: 300,
   },
-  blue: {
-    backgroundColor: 'blue',
-    // flex: 3
+  input: {
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    width: "100%",
   },
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'space-around',
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    alignItems: "stretch",
+    justifyContent: "center",
+    paddingTop: 22
+  },
+  ScrollView: {
+    width: Dimensions.get("window").width,
+  },
+  item: {
+    padding: 10,
+    fontSize: 22,
+    height: 50,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
   },
 });
